431acc1855917a9edd7848fd12d83d3c519d3fda
sampso29 <37120100+sampso29@users.noreply.github.com>
2018-04-01 11:35:44 -0400

    bare bone sign in page design (no php yet)
    sign in design and stylesheet

---
+++ htdocs/index.php
@@ -0,0 +1,25 @@
+
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="SignIn.css">
+    <title> CSCB20 SignIn </title>
+  </head>
+
+  <body>
+    <div id="CourseCode"><p>CSCB20</p></div>
+    <div id="CourseName"><p>Introduction to Databases and Web Applications | Winter 2018</p></div>
+    <br>
+    <div class = 'SignInBox'>
+      <div>Login</div>
+      <form method = 'post'>
+        <div id="Username">Username <input type="text" name="User" placeholder="Enter UTor Username"></div>
+        <br>
+        <div id="Password ">Password <input type="text" name="Pass" placeholder="Enter UTor Password"></div>
+        <br>
+        <div id="Submit"><input type="submit" Value="submit"></div>
+      </form>
+
+    </div>
+  </body>
+
+</html>
