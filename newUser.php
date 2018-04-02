2a8b65c6ea1ae61814d273346f64af52369df343
sampso29 <37120100+sampso29@users.noreply.github.com>
2018-04-01 20:18:17 -0400

    New User Page

--- newUser.php
+++ newUser.php
@@ -1,10 +1,30 @@
 <html>
   <head>
+    <link rel="stylesheet" type="text/css" href="SignIn.css">
     <title>New User</title>
   </head>
 
   <body>
-    <form action = '' method = 'post'>
-    </form>
+    <div class="NewUserBox">
+      <form action = '' method = 'post'>
+        <div class='UName'>
+          First Name: <input type="text" name="NewUFname" placeholder="Enter First Name As Seen On TCard">
+          Last Name: <input type="text" name="NewULname" placeholder="Enter Last Name As Seen On TCard">
+        </div>
+
+        <div class='AccType'>
+          <span><input type="radio" name="AccType" value="Student"></span>
+          <span><input type="radio" name="AccType" value="TA"></span>
+          <span><input type="radio" name="AccType" value="Instructor"></span>
+        </br>
+        </div>
+
+        <div class="AccInfo">
+          Username: <input type="text" name="NewUserN" value="Enter Acorn Username">
+          Password: <input type="text" name="NewUPass" value="Enter New Account Password">
+        </div>
+      </form>
+  </div>
+
   </body>
 </html>
